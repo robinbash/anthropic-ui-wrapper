6f1a27646e17bbabec82e4cb1598c599f46ae33f
robinbash <huluvublue@gmail.com>
2024-06-26 17:45:28 +0200

    send message

--- src/lib/components/Convo.svelte
+++ src/lib/components/Convo.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
+  import Anthropic from "@anthropic-ai/sdk";
   import { currentConvoId } from "../stores";
+
+  let text = "";
   let inputEl: HTMLDivElement;
+  const client = new Anthropic({
+    apiKey: "",
+  });
+
+  const submitMessage = async () => {
+    const message = inputEl.innerText;
+    if (!message) return;
+    console.log(message);
+    client.messages
+      .stream({
+        messages: [{ role: "user", content: "Hello" }],
+        model: "claude-3-5-sonnet-20240620",
+        max_tokens: 1024,
+      })
+      .on("text", (newText) => {
+        text = newText;
+      });
+  };
 </script>
 
 <div class="convo">
-  <div class="messages"></div>
+  <div class="messages">{text}</div>
   <div class="bottom">
-    <!-- <textarea />  -->
     <div class="input" contentEditable bind:this={inputEl} />
     <div>
-      <button
-        on:click={() => {
-          console.log(inputEl.innerText);
-        }}>{">"}</button
-      >
+      <button on:click={submitMessage}>{">"}</button>
     </div>
   </div>
 </div>
@@ -25,6 +41,7 @@
     width: 100%;
   }
   .messages {
+    padding: 3rem;
     height: 100%;
   }
   .bottom {
--- src/lib/stores/convoStore.ts
+++ src/lib/stores/convoStore.ts
@@ -1,3 +1,3 @@
 import { writable } from "svelte/store";
 
-export const currentConvoId = writable<string>();
+export const currentConvoId = writable<string | null>(null);
